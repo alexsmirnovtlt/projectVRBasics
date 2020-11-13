e40e2c12e5a0be6297d19085cd5d7ddbb199af04
Alexander Smirnov <smirnov@lb24.ru>
2020-11-14 00:06:44 +0400

    initial commit

---
+++ Source/projectVRBasics/projectVRBasicsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "projectVRBasicsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVRBASICS_API AprojectVRBasicsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
